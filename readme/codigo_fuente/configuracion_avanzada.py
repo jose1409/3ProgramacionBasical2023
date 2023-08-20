0d5b472d1659c10e6f7c2f854a27f82ddaf7e94c
gizulin <guisellecastro014@gmail.com>
2023-08-20 14:33:34 -0600

    finalizacion de configuaciones avanzadas

--- readme/codigo_fuente/configuracion_avanzada.py
+++ readme/codigo_fuente/configuracion_avanzada.py
@@ -1,14 +1,16 @@
+"Este es el menu de configuraciones avanzadas el cual se despliega del menu principal y permite la eliminacion de usuarios y modificacion de valores"
 import getpass
 import os
 
 # PIN especial definido por el equipo para permitir acceso al menú
 specialPIN = "2023"
-usuarios_pines_file = "usuarios_pines.txt"
+usuarios_pines_file = "readme/usuarios_pines.txt"
+configuraciones_avanzadas_file = "readme/configuraciones_avanzadas.txt"
 
 def getUserInput(prompt):
     return input(prompt).strip()
 
-def getHiddenInput(prompt): # ocultar el PIN de la pantalla
+def getHiddenInput(prompt):
     return getpass.getpass(prompt)
 
 def authenticateUser():
@@ -19,7 +21,8 @@ def authenticateUser():
             return True
         attempts -= 1
         print("El PIN es incorrecto. Sus intentos restantes son:", attempts)
-    return False #probar el PIN especial
+    return False
+
 
 def cargar_usuarios_pines(file_path):
     usuarios_pines = {}
@@ -61,6 +64,24 @@ def deleteUser(userID):
     else:
         print(f"El ID {userID} es incorrecto o no existe en la lista de usuarios. Intente nuevamente.")
 
+
+def cargar_configuraciones_avanzadas(file_path):
+    configuraciones = {}
+    try:
+        with open(file_path, 'r') as file: #leer archivo en modo lectura
+            lines = file.readlines()
+            for line in lines: #recorrer cada linea
+                key, value = line.strip().split(":") #eliminar espacios en blanco
+                configuraciones[key] = value
+    except FileNotFoundError:
+        pass
+    return configuraciones
+
+def guardar_configuraciones_avanzadas(file_path, configuraciones):
+    with open(file_path, 'w') as file:
+        for key, value in configuraciones.items():
+            file.write(f"{key}:{value}\n")
+
 def modifySystemValues():
     conversionOptions = [
         "Tipo de cambio: Compra de dólares usando colones",
@@ -71,27 +92,33 @@ def modifySystemValues():
         "Inversión mínima para registrarse",
         "Salir"
     ]
+
+    configuraciones = cargar_configuraciones_avanzadas(configuraciones_avanzadas_file)
+
     while True:
         print("¿Qué desea modificar el día de hoy?")
         for idx, option in enumerate(conversionOptions, start=1):
             print(f"{idx}. {option}")
-        
+
         try:
             choice = int(getUserInput("Ingrese el número de la opción que desea modificar: "))
-        
-            if 1 <= choice <= len(conversionOptions) - 1:
-                newValue = getUserInput("Ingrese el nuevo valor: ")
-                # implementación para modificar los valores del sistema
-                # funcion que abrá el archivo
-                
-                print(f"El siguiente Valor ha sido modificado para la opción {choice}: {newValue}")
-            elif choice == len(conversionOptions):
-                print("Volviendo al menú principal.")
-                break
+
+            if 1 <= choice <= len(conversionOptions)-1:
+                option_to_modify = conversionOptions[choice - 1]
+                current_value = configuraciones.get(option_to_modify, "No se encontró valor")
+                print(f"Valor actual para {option_to_modify}: ${current_value}")
+
+                new_value = getUserInput("Ingrese el nuevo valor: ")
+                configuraciones[option_to_modify] = new_value
+                guardar_configuraciones_avanzadas(configuraciones_avanzadas_file, configuraciones)
+
+                print(f"El valor para {option_to_modify} ha sido modificado exitosamente.")
+            elif choice == len(conversionOptions) :
+                return
             else:
                 print("Opción inválida.")
-        except ValueError:
-            print("Error: Debes ingresar un número entero. Intente nuevamente.")
+        except Exception as e:
+            print("Error: Debes ingresar un número entero. Intente nuevamente." + e)
 
 def mainMenu():
     while True:
@@ -111,11 +138,11 @@ def mainMenu():
                 modifySystemValues()
             elif choice == 3:
                 print("Saliendo del programa.")
-                exit()
+                return
             else:
                 print("Opción inválida. Vuelva a intentarlo.")
-        except ValueError:
-            print("Error: Debes ingresar un número entero. Intente nuevamente.")
+        except Exception as e:
+            print("Error: Debes ingresar un número entero. Intente nuevamente." + e)
 
 def menu_configuracion_avanzada():
     print("Este es el menú de Configuración Avanzada. Autenticación es requerida.")
@@ -126,7 +153,3 @@ def menu_configuracion_avanzada():
 
 if __name__ == "__main__":
     menu_configuracion_avanzada()
-
-
-
-
